bcf84716e0fcc74bdae9542f315cbd0c94afaa3b
Sbotix <52281317+Sbotix@users.noreply.github.com>
2020-06-15 12:16:34 +0200

    v.félix1

---
+++ module1.py
@@ -0,0 +1,17 @@
+﻿import random
+def grotte():
+    id1=["chaude","froide","bonne"]
+    id2=["humide","sec","bonne"]
+    id3=["peuplé d'orc","peuplé d'elfe noir","peuplé d'elf noir","peuplé de nain","peuplé de gobelin","peuplé d'insectes","inhabité","peuplé de chauve souris","inhabité","inhabité","peuplé d'insectes","peuplé de chauve souris","peuplé d'insectes","inhabité"]
+    a=random.choice(id1)
+    b=random.choice(id2)
+    c=random.choice(id3)
+    print("La température est " + a + " et l'humidité est " + b + ", la grotte est " + c + ".")
+
+
+print("1=grotte")
+a=(str(input("que choississez vous? ")))
+if (str(a==1)):
+    grotte()
+else:
+    print("choissisez parmis la liste s'il vous plaits.")
